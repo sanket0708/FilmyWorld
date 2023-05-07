c6db7b9b33d3ca03061616e0ad36d74687f58622
Sanket <sanketmudhulkar98@gmail.com>
2023-05-08 02:03:43 +0530

    Footer completed

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const SwitchTabs = ({ data, onTabChange }) => {
+  return <div className="switchingTabs">SwitchTabs</div>;
+};
+
+export default SwitchTabs;
